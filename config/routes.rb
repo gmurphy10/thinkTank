e7c8a7c733425ee97af1d9be14f3adbd04f4590b
george murphy <georgeolivermurphy@hotmail.com>
2021-05-13 17:54:17 +0100

    root pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
+
   get '/about', to: "pages#about"
   #verb         to    controller#action
-
   get '/profile', to: "pages#profile"
 
 end
